8b8575354ad7ad89714865eb4078f46595c9325b
toby959/toto.git <134654437+toby959@users.noreply.github.com>
2024-03-04 17:41:45 -0300

    Subiendo a IntelliJ

---
+++ Main.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+  /* System.out.println("Hola Tobias");
+        System.out.println("\n");
+        System.out.println("* Toby");
+        System.out.println("/////////////////////////////////");
+
+        String nombre = "Toto";
+        Integer nroEquipo = 1;
+        String texto = "Hola, soy " + nombre + ", del equipo nro. " + nroEquipo;
+        System.out.println(texto);
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+/*
+        System.out.println("Que edad tienes?");
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextInt();
+
+       // Integer edad = 33;
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
+*/
+        //   System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+     /*   //Peso en Kg / altura en metros al cuadrado
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Que peso tienes en Kg");
+        double peso = sc.nextDouble(); //90.5;
+        //sc.nextLine();
+        System.out.println("Ingresa tu altura en metros");
+        //int edad = sc.nextInt();
+        double altura = sc.nextDouble();//1.85;
+        //sc.nextLine();
+        double indiceMasaCorporal = peso / (altura * altura);
+        System.out.println("Tu indice de masa corporal es: " + indiceMasaCorporal);
+
+        // Mayor 30 -> obsesidad
+        if(indiceMasaCorporal >= 30){
+            System.out.println("Estás en estado obesidad!!!");
+        }
+        // Entre 25 y 30 -> sobrepeso
+        if(indiceMasaCorporal >= 25 && indiceMasaCorporal < 30){
+            System.out.println("Estás en sobrepeso");
+        }
+        // Entre 18.5 y 25 -> normal
+        if(indiceMasaCorporal >= 18.5 && indiceMasaCorporal < 25)
+            System.out.println("Estás en el peso normal!!!");
+        // Menor a 18.6 -> debajo del normal
+        if(indiceMasaCorporal < 18.6){
+            System.out.println("Estás con el peso por debajo de lo normal");
+        }
+*//*
+        System.out.println("##################################################");
+        Scanner cargaDeDatos = new Scanner(System.in);
+
+        // Peso en KG / altura en metros al cuadrado
+        System.out.println("Ingrese su peso en KG");
+        double peso = cargaDeDatos.nextDouble();
+        System.out.println("Ingrese su altura en metros");
+        double altura = cargaDeDatos.nextDouble();
+
+        double indiceMasaCorporal = peso / (altura * altura);
+        System.out.println("Tu indice de masa corporal es: " + indiceMasaCorporal);
+
+        // Mayor a 30 -> Obesidad
+        if (indiceMasaCorporal >= 30) {
+            System.out.println("Estás en estado de obesidad.");
+        }
+        // Entre 25 y 30 sobrepeso
+        if (indiceMasaCorporal >= 25 && indiceMasaCorporal < 30) {
+            System.out.println("Estás en estado de sobrepeso.");
+        }
+        // Entre 18.5 y 25 normal
+        if (indiceMasaCorporal >= 18.5 && indiceMasaCorporal < 25) {
+            System.out.println("Estás en un peso normal.");
+        }
+        // Menor a 18.6 debajo del normal
+        if (indiceMasaCorporal < 18.6) {
+            System.out.println("Estás en un peso menor al normal.");
+        }
+*/
+        System.out.println("##################################################");
+        Scanner cargaEdad = new Scanner(System.in);
+        System.out.println("Ingresa tu edad humano");
+
+        Integer edad = cargaEdad.nextInt();
+        Integer edadCat = edad * 7;
+        System.out.println("La edad de tu Cat es: " + edadCat);
+        System.out.println("##################################################");
+
+        Scanner MillasAKilometor = new Scanner(System.in);
+        System.out.println("Ingresa una distancia en Millas");
+
+        double millas = MillasAKilometor.nextDouble();
+        double kilometros = millas * 1.60934;
+
+        System.out.println("La distancia en Kilometros es: " + kilometros);
+
+        System.out.println("##################################################");
+//Precio
+//descuento
+//Precio con descuento
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el valor el producto: ");
+        double precio = sc.nextDouble();
+
+        System.out.println("Ingresa el valor de descuento: ");
+        double descuento = sc.nextDouble();
+
+        double PrecioFinal = precio - (precio * descuento  / 100);
+
+        System.out.println("El valor del producto con descuento  es: " + PrecioFinal);
+
+        System.out.println("##################################################");
+
+    }
+}
+
+
+
